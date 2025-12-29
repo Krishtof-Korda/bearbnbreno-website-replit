ae0a9a26b4534d46bb11a9a9c7cad5add3db0a24
kbeark <52060741-kbeark@users.noreply.replit.com>
2025-12-29 22:33:48 +0000

    Restored to 'ebe365dd1e3e0854ff2493ba7ea841f3db62f2de'
    Replit-Restored-To: ebe365dd1e3e0854ff2493ba7ea841f3db62f2de

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,14 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route>
-        {() => {
-           // Redirect to root if not found since we are consolidating
-           window.location.href = "/";
-           return null;
-        }}
-      </Route>
+      <Route component={NotFound} />
     </Switch>
   );
 }
